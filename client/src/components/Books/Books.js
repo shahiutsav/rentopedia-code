eed47f6baee75eace2ac9f03ab4f13f15a813a1d
Utsav Shahi <shahiutsav180@gmail.com>
2022-05-25 15:12:05 +0545

    Add dynamic metadata for different pages
    A Metadata file was created that can be used for generating varieties of metadata for different pages.

--- client/src/components/Books/Books.js
+++ client/src/components/Books/Books.js
@@ -1,32 +1,36 @@
-import React from "react"
-import "./Books.css"
-import dummyImage from "../images/lf.jpeg"
+import React, { Fragment } from "react";
+import "./Books.css";
+import dummyImage from "../images/lf.jpeg";
 
-import BookCard from "./BookCard"
+import BookCard from "./BookCard";
+import MetaData from "../layout/Metadata";
 
 const book = {
     name: "The Book Thief",
     images: [{ url: dummyImage }],
     price: "NRs. 1200",
     _id: "dummyBook",
-}
+};
 
 const Book = () => {
     return (
-        <div className="small-container">
-            <h2 className="title">Featured Books</h2>
-            <div className="row">
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
-                <BookCard book={book} />
+        <Fragment>
+            <MetaData title="Rentopedia" />
+            <div className="small-container">
+                <h2 className="title">Featured Books</h2>
+                <div className="row">
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                    <BookCard book={book} />
+                </div>
             </div>
-        </div>
-    )
-}
+        </Fragment>
+    );
+};
 
-export default Book
+export default Book;
