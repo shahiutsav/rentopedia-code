b49268b35d402a1bf3c0378b6e4a2ef32642639d
Utsav Shahi <shahiutsav180@gmail.com>
2022-05-25 14:47:54 +0545

    Add Footer for webpage
    A basic responsive footer was created which changes its display based on different screen sizes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./components/layout/Footer/Footer";
 
 function App() {
-    return <Navbar />;
+    return (
+        <Router>
+            <Navbar />
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/components/layout/Footer/Footer.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        <h4>Rentopedia</h4>
+                        <ul>
+                            <li>
+                                <a href="#">About us</a>
+                            </li>
+                            <li>
+                                <a href="#">Our Services</a>
+                            </li>
+                            <li>
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>Support</h4>
+                        <ul>
+                            <li>
+                                <a href="#">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="#">Shipping</a>
+                            </li>
+                            <li>
+                                <a href="#">Returns</a>
+                            </li>
+                            <li>
+                                <a href="#">Order Status</a>
+                            </li>
+                            <li>
+                                <a href="#">Payment Options</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>Cart</h4>
+                        <div class="social-links">
+                            <a href="#">
+                                <i className="fab fa-instagram"></i>
+                            </a>
+
+                            <a href="#">
+                                <i className="fab fa-facebook"></i>
+                            </a>
+
+                            <a href="#">
+                                <i className="fab fa-youtube"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
