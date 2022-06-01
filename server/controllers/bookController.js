4b8d7948c4dfc2559479b3d42ce1c42a999ceda2
Grishma Shrestha <grishmastha2000@gmail.com>
2022-06-01 12:11:12 +0545

    Implement backend search and filter function
    apiFeatures file was created to handle features like searching and
    filtering books from the website. The created features were implemented
    on the bookController.

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,6 +1,7 @@
 const Book = require("../models/bookModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apiFeatures");
 
 // Create Book -- Admin
 exports.createBook = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,10 @@ exports.createBook = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Books
 exports.getAllBooks = catchAsyncErrors(async (req, res) => {
-    const books = await Book.find();
+    const apiFeature = new ApiFeatures(Book.find(), req.query)
+        .search()
+        .filter();
+    const books = await apiFeature.query;
     res.status(200).json({
         success: true,
         books,
