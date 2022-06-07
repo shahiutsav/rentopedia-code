ecb3d59bf2b46cc758050b728b887df3dde1f1f9
Utsav Shahi <shahiutsav180@gmail.com>
2022-06-07 17:40:48 +0545

    Add userModel and enable user registration
    A userModel was created inside the models directory of server which defines the data that needs to be inserted. Next a userController was created to handle the user registration. This can enable users to register themselves to the Rentopedia Database.

--- server/app.js
+++ server/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 // Route imports
 const book = require("./routes/bookRoute");
+const user = require("./routes/userRoute");
 
 app.use("/api/v1", book);
+app.use("/api/v1", user);
 
 // Middleware for Errors
 app.use(errorMiddleware);
--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const bookSchema = mongoose.Schema({
+const bookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "Please enter the name of the Book"],
