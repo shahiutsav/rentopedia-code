ae867b644ba67828ef078227bcbfd34bb4a2505e
Utsav Shahi <shahiutsav180@gmail.com>
2022-06-07 17:57:24 +0545

    Add User Login and authentication
    Function for user to login to the system has been added and authentication in the required pages has also been added.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,3 +18,27 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 201, res);
 });
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    // checking if user has provided both email and password
+
+    if (!email || !password) {
+        return next(new ErrorHandler("Please enter email & password"));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    sendToken(user, 200, res);
+});
