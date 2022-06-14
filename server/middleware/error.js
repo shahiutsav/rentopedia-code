97c566e0937956da42d2f399bc0dea695b064799
Utsav Shahi <shahiutsav180@gmail.com>
2022-06-14 17:00:36 +0545

    Handle errors for duplicate keys, invalid JWT and expire JWT
    The error.js file inside middleware now handles error for duplicate keys trying to be registered, wrong JWT or expired JWT. It sends user the information for the cause of the error.

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -11,6 +11,28 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    // Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `The provided ${Object.keys(
+            err.keyValue
+        )} has already been registered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token is invalid, please try again`;
+
+        err = new ErrorHandler(message, 400);
+    }
+
+    // JWT EXPIRE error
+    if (err.name === "TokenExpiredError") {
+        const message = `Json Web Token is expired, please try again`;
+
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
