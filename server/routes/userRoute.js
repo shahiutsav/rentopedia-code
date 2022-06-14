aab3b27e01c3bed1540ad03dd3a634f118a294ab
Utsav Shahi <shahiutsav180@gmail.com>
2022-06-14 17:14:28 +0545

    Add update password feature in server

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,6 +6,7 @@ const {
     forgotPassword,
     resetPassword,
     getUserDetails,
+    updatePassword,
 } = require("../controllers/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
@@ -16,5 +17,6 @@ router.route("/logout").get(logout);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
